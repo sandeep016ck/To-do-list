26492f5a8af9d06ab8b0d1e199f282ed9d895ff3
sandeepsamanto <sandeepsamanto7@gmaill.com>
2025-01-09 23:38:28 +0530

    Changed some Routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express=require("express");
-const app=express()
+const app=express();
+const cors = require("cors");
 const dotenv=require("dotenv");
 const connectdb = require("./config/dbconnection");
 
 dotenv.config();
 app.use(express.json());
+app.use(cors())
 const port=process.env.PORT; 
-
-
 
 connectdb();
 app.use('/api/to-do/user',require("./routes/userRoutes"));
